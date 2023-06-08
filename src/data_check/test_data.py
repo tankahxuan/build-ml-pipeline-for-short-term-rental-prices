bf90c235453abba428f0270d90020d37c0438319
KAHXUAN <62658882+tankahxuan@users.noreply.github.com>
2023-06-08 09:48:58 +0800

    Update test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,9 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
-def test_row_count(data):
+def test_row_count(data: pd.DataFrame):
+    """
+    To test whether the dataset has sufficient data points for analysis. 
+    """
     assert 15000 < data.shape[0] < 1000000
 
 def test_price_range(data: pd.DataFrame, min_price: float, max_price:float):
+    """
+    Test proper price that is within range for house properities.
+    """
     tmp = data['price'].between(min_price, max_price)
     assert tmp.shape[0]==data.shape[0]
